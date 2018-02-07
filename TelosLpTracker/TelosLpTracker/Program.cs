4bd1fdb43a69a6eab48a6438b997cfaeda611d33
Anthony Pham <anthonytpham31@gmail.com>
2018-02-06 21:54:55 -0700

    Simple Telos Console App

---
+++ TelosLpTracker/TelosLpTracker/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace TelosLpTracker
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var repeat = true;
+            while (repeat)
+            {
+                var enrage = EnrageInput();
+                var telosCalculations = new TelosHealth(enrage);
+                OutputTelosHealth(telosCalculations);
+                repeat = AskToPlayAgain();
+            }
+        }
+
+        private static void OutputTelosHealth(TelosHealth telosCalculations)
+        {
+            Console.WriteLine("Phase One: " + telosCalculations.StartingHealth);
+            Console.WriteLine("Phase Two: " + telosCalculations.PhaseTwoHealth());
+            Console.WriteLine("Phase Three: " + telosCalculations.PhaseThreeHealth());
+            Console.WriteLine("Phase Four: " + telosCalculations.PhaseFourStartingHealth);
+            Console.WriteLine("Phase Four F1: " + telosCalculations.PhaseFourFontOneHealth());
+            Console.WriteLine("Phase Four F2: " + telosCalculations.PhaseFourFontTwoHealth());
+            Console.WriteLine("Phase Four F3: " + telosCalculations.PhaseFourFontThreeHealth());
+        }
+
+        private static double EnrageInput()
+        {
+            while (true)
+            {
+                Console.WriteLine("Input Enrage: ");
+                var enrageInput = Console.ReadLine();
+
+                if (CheckEnrageInput(enrageInput)) return double.Parse(enrageInput);
+            }
+        }
+
+        private static bool CheckEnrageInput(string enrageInput)
+        {
+            return enrageInput.All(char.IsNumber);
+        }
+
+        private static bool AskToPlayAgain()
+        {
+            Console.WriteLine("Press Enter or Y to Play Again.");
+            var repeatInput = Console.ReadLine()?.ToLower();
+            Console.Clear();
+            return repeatInput == "y" || repeatInput == "";
+        }
+    }
+}
---
+++ TelosLpTracker/TelosLpTracker/TelosHealth.cs
@@ -0,0 +1,52 @@
+﻿namespace TelosLpTracker
+{
+    public class TelosHealth
+    {
+        private double Enrage { get; }
+        public double StartingHealth { get; }
+        public double PhaseFourStartingHealth { get; }
+
+        public TelosHealth(double enrage)
+        {
+            Enrage = enrage;
+            StartingHealth = PhaseOneHealth();
+            PhaseFourStartingHealth = PhaseFourHealth();
+        }
+
+        public double PhaseOneHealth()
+        {
+            if (Enrage > 200) return 600000;
+            return 400000 + Enrage * 1000;
+        }
+
+        public double PhaseTwoHealth()
+        {
+            return StartingHealth - StartingHealth * 0.25;
+        }
+
+        public double PhaseThreeHealth()
+        {
+            return StartingHealth - StartingHealth * 0.5;
+        }
+
+        public double PhaseFourHealth()
+        {
+            return StartingHealth - StartingHealth * 0.75;
+        }
+
+        public double PhaseFourFontOneHealth()
+        {
+            return PhaseFourStartingHealth - PhaseFourStartingHealth * 0.25;
+        }
+
+        public double PhaseFourFontTwoHealth()
+        {
+            return PhaseFourStartingHealth - PhaseFourStartingHealth * 0.5;
+        }
+
+        public double PhaseFourFontThreeHealth()
+        {
+            return PhaseFourStartingHealth - PhaseFourStartingHealth * 0.75;
+        }
+    }
+}
